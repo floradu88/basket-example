d09722ea3694bf5cb8ffbcc0f8e591a8ee7efbad
Radu Florescu <radu.florescu@hotmail.com>
2018-10-23 10:48:59 +0300

    Added initial project structure.

---
+++ basket-example.tests/BasketTests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace basket_example.tests
+{
+    
+    public class BasketTests
+    {
+        [Fact]
+        public void should_be_able_to_create_a_basket()
+        {
+            var basket = new Basket();
+        }
+    }
+}
